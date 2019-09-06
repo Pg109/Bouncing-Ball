c75d0371f26979ddb677a5362ce838178766d3be
J Ng <john.k.ng@gmail.com>
2019-09-06 16:42:05 +0100

    Code at last

---
+++ Assets/BallController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public int speed; 
+    Rigidbody ballRigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+        ballRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log( Input.GetAxis("Horizontal") );
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        // This text is igmored by the computer.
+        // This is only for us.
+        // Yo.
+        // Now. We add the directiom as a Force to the RigidBody.
+
+        ballRigidBody. AddForce(direction * speed);
+
+    }
+}
